ba809ef35894d5044c839286c48a9eba7b7e4b14
ricardo.castilloacevedo@ucr.ac.cr <ricardo.castilloacevedo@ucr.ac.cr>
2026-04-28 15:24:19 -0600

    Se crea el AppDbContext e instalación del EntityFrameworkCore

---
+++ Producto/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Producto.Models;
+
+namespace Producto.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Producto.Models.Producto> Productos { get; set; }
+    }
+}
