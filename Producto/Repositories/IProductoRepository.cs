3f46f4539b23ce7870325cd5fa79df5555bf41ff
ricardo.castilloacevedo@ucr.ac.cr <ricardo.castilloacevedo@ucr.ac.cr>
2026-04-28 15:41:42 -0600

    Se hacen cambios e las interfaces

--- Producto/Repositories/IProductoRepository.cs
+++ Producto/Repositories/IProductoRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Producto.Repositories
+﻿using System.Collections.Generic;
+using Producto.Models;
+
+namespace Producto.Repositories
 {
     public interface IProductoRepository
     {
+        IEnumerable<global::Producto.Models.Producto> ObtenerTodos();
+        global::Producto.Models.Producto ObtenerPorId(int id);
+        void Agregar(global::Producto.Models.Producto p);
+        void Actualizar(global::Producto.Models.Producto p);
+        void Eliminar(int id);
     }
 }
--- Producto/Repositories/ProductoRepository.cs
+++ Producto/Repositories/ProductoRepository.cs
@@ -1,6 +1,49 @@
-﻿namespace Producto.Repositories
+﻿using System.Collections.Generic;
+using System.Linq;
+using Producto.Data;
+using Producto.Models;
+
+namespace Producto.Repositories
 {
-    public class ProductoRepository
+    public class ProductoRepository : IProductoRepository
     {
+        private readonly AppDbContext _context;
+
+        public ProductoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Producto.Models.Producto> ObtenerTodos()
+        {
+            return _context.Productos.ToList();
+        }
+
+        public Producto.Models.Producto ObtenerPorId(int id)
+        {
+            return _context.Productos.Find(id);
+        }
+
+        public void Agregar(Producto.Models.Producto p)
+        {
+            _context.Productos.Add(p);
+            _context.SaveChanges();
+        }
+
+        public void Actualizar(Producto.Models.Producto p)
+        {
+            _context.Productos.Update(p);
+            _context.SaveChanges();
+        }
+
+        public void Eliminar(int id)
+        {
+            var producto = _context.Productos.Find(id);
+            if (producto != null)
+            {
+                _context.Productos.Remove(producto);
+                _context.SaveChanges();
+            }
+        }
     }
 }
