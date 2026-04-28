65723719593cbb7f245155514681fba9e4bd4dc6
ricardo.castilloacevedo@ucr.ac.cr <ricardo.castilloacevedo@ucr.ac.cr>
2026-04-28 15:23:33 -0600

    Se crea los modelos y requerimientos

--- Producto/Models/Producto.cs
+++ Producto/Models/Producto.cs
@@ -1,7 +1,25 @@
-﻿namespace Producto.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Producto.Models
 {
     public class Producto
     {
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        [Range(1, 9999999, ErrorMessage = "El precio debe ser mayor a 0")]
+        public decimal Precio { get; set; }
+
+        [Required(ErrorMessage = "La categoría es obligatoria")]
+        public string Categoria { get; set; }
+
+        public bool Activo { get; set; }
+
+        public DateTime FechaIngreso { get; set; } = DateTime.Now;
     }
 }
